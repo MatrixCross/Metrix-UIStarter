92d63d075e63082469e36062c99a10153e89e044
Mr.Li <63112788+undercurre@users.noreply.github.com>
2023-01-14 18:58:56 +0800

    Initial commit

---
+++ packages/components/es/index.js
@@ -0,0 +1,7 @@
+import { default as default2 } from "./button/index.vue.js";
+import { testfun } from "./utils/index.js";
+const result = testfun(1, 1);
+console.log(result);
+export {
+  default2 as Button
+};
---
+++ packages/components/src/index.ts
@@ -0,0 +1,11 @@
+import Button from './button'
+
+export {
+    Button
+}
+
+import {testfun} from '@metu-ui/utils'
+
+const result = testfun (1,1)
+
+console.log(result)
